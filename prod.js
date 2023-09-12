cf4b8930a7f56edf88dfc581ceacd073e663a870
Sallonii <121022481+Sallonii@users.noreply.github.com>
2023-09-12 10:55:34 +0530

    Initial commit

---
+++ prod.js
@@ -0,0 +1,457 @@
+
+// JSON data of products
+let hardcodedJSON = {
+    "dogs": {
+        "toys": [ {
+            "img": "https://headsupfortails.com/cdn/shop/products/TrixieNeonBallDogToy_Assorted_204bfe4b-ba7e-458b-9042-7031b1fd430e.jpg?v=1663051434&width=360",
+                "name": "Trixie Neon Ball",
+                "rating": 4.5,
+                "price": 171
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/GiGwiBallSqueakerDogChewToy-solidtransparent-BlueOrange.jpg?v=1662974656&width=360",
+                "name": "GiGwi Ball",
+                "rating": 4.82,
+                "price": 375
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/TrixieNaturalDentaFunBall_MintFlavourToyForDogs-Green.jpg?v=1662984472&width=360",
+                "name": "Trixie Ball on Rope Dog Toy",
+                "rating": 5.0,
+                "price": 324
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/HUFTQuintessentialRubberFootballDogToy.jpg?v=1663833183&width=360",
+                "name": "KONG Ball",
+                "rating": 5.0,
+                "price": 1099
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/TrixieSoftCanvasSoccerBallDogToy-Soundless-11cm.jpg?v=1662984566&width=360",
+                "name": "Outward Hong Ball",
+                "rating": 3.6,
+                "price": 225
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/TrixieCatnipBallwithBellCatToy_dbb2487f-25de-45b6-a9cc-baa58fa8a5e6.jpg?v=1663063847&width=360",
+                "name": "Belly Ball",
+                "rating": 4.6,
+                "price": 325
+        }
+
+        ],
+        "clothing": [ {
+            "img": "https://headsupfortails.com/cdn/shop/products/HUFT-Wintersong-Corduroy-Dog-Jacket---Blue.jpg?v=1668688899&width=360",
+                "name": "HUFT Wintersong Jacket",
+                "rating": 5.0,
+                "price": 1169
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/DSC_0133-Edit_1.jpg?v=1668402797&width=360",
+                "name": "HUFT Tweedle Dee Dog Jacket",
+                "rating": 5.0,
+                "price": 2159
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/DSC_4343-Edit.jpg?v=1670164384&width=360",
+                "name": "HUFT Reversible Jacket - Red",
+                "rating": 4.0,
+                "price": 2240
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/DSC_1485-Edit.jpg?v=1668666807&width=360",
+                "name": "HUFT Grrberry Jacket",
+                "rating": 5.0,
+                "price": 1599
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/DSC_2110-Edit.jpg?v=1667286108&width=360",
+                "name": "HUFT Luxe Stud ",
+                "rating": 3.6,
+                "price": 3251
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/DSC_0395-Edit_2521bccb-8f93-438c-92d7-d9e19237950c.jpg?v=1671530607&width=360",
+                "name": "Cosy Pupper Reversible Jacket",
+                "rating": 5.0,
+                "price": 1250
+        }
+
+        ]
+    }
+
+    ,
+    "cat": {
+        "catBedding": [ {
+            "img": "https://headsupfortails.com/cdn/shop/products/HUFT-Personalised-Cosy-Puppy-Cat-Bed---Lilac.jpg?v=1671449594&width=360",
+                "name": "Cosy Cat Bed",
+                "rating": 4.5,
+                "price": 1710
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/DSC_9554_6b89c6e8-8e34-4745-bc1b-8cc78407fe6e.jpg?v=1679565212&width=360",
+                "name": "Cloudy Bed",
+                "rating": 4.82,
+                "price": 3750
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/files/4011905369716.jpg?v=1683704715&width=360",
+                "name": "Cuddly Round Bed",
+                "rating": 5.0,
+                "price": 3240
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/Trixie-Liva-Cuddly-Cave-Cat-Bed---16x19x10-inch.jpg?v=1662197684&width=360",
+                "name": "Cave Cat Bed",
+                "rating": 5.0,
+                "price": 2099
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/HUFT-Personalised-Cosy-Puppy-Cat-Bed---Red.jpg?v=1671449607&width=360",
+                "name": "Cosy Red Bed",
+                "rating": 3.6,
+                "price": 2250
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/Trixie-Luna-Cuddly-Cave-Cat-Bed-_40X24X46-cm_---Grey.jpg?v=1662197719&width=360",
+                "name": "Grey Cave Bed",
+                "rating": 4.6,
+                "price": 3250
+        }
+
+        ],
+        "catBowls": [ {
+            "img": "https://headsupfortails.com/cdn/shop/files/HUFTCatFaceDoubleDiner_Pink-HUFTBW274.jpg?v=1684393099&width=360",
+                "name": "Pink Bowl",
+                "rating": 5.0,
+                "price": 169
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/files/HUFTBW277.jpg?v=1688461154&width=360",
+                "name": "Solid Bowl",
+                "rating": 5.0,
+                "price": 215
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/Trixie-Multi-Colour-Plastic-Bowl-For-Cats-Non-Slip-200-ml-11cm-Diameter.jpg?v=1658406936&width=360",
+                "name": "Multi Color Bowl",
+                "rating": 4.0,
+                "price": 224
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/files/HUFTSuctionCatBowl-HUFTBW273.jpg?v=1684481203&width=360",
+                "name": "Cute Cat Bowl",
+                "rating": 4.8,
+                "price": 599
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/files/DSC_7816_fd3e4bb6-e67f-4604-af18-beb099357c1f.jpg?v=1684409181&width=360",
+                "name": "HUFT Bowl",
+                "rating": 3.6,
+                "price": 325
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/files/HUFTCatFaceDoubleDiner_Blue-HUFTBW273.jpg?v=1684394011&width=360",
+                "name": "Double Dinner Bowl",
+                "rating": 5.0,
+                "price": 125
+        }
+
+        ]
+    }
+
+    ,
+    "smallAnimals": {
+        "rabbit": [ {
+            "img": "https://headsupfortails.com/cdn/shop/products/ZupreemNaturesPromiseTimothyNaturalsGuineaPigFood-2.26kg.jpg?v=1659508541",
+                "name": "Natural Food",
+                "rating": 4.5,
+                "price": 1210
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/ZVP-0136_Economic_swinka_2013.jpg?v=1654583374&width=360",
+                "name": "Economic Food",
+                "rating": 4.82,
+                "price": 1250
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/ZupreemNature_sPromiseTimothyNaturalsRabbitDryFood-1.6kg.jpg?v=1659508528&width=360",
+                "name": "Nature's Promise",
+                "rating": 5.0,
+                "price": 2009
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/HarringtonsSmallAnimalOptimumGuineaPigFood-2kg.jpg?v=1639630978&width=360",
+                "name": "Guinea Food",
+                "rating": 5.0,
+                "price": 1435
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/ZuPreem-Natures-Promise-Timothy-Naturals-Rabbit-Food-768x768_1_cc50eea4-21e2-4343-a179-3595f8096046.jpg?v=1659426230&width=360",
+                "name": "Timothy Naturals",
+                "rating": 3.6,
+                "price": 1004
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/ZVP-0126_Economic_krolik_2013kopia.jpg?v=1654583043&width=360",
+                "name": "Vitapol Food",
+                "rating": 4.6,
+                "price": 1333
+        }
+
+        ],
+        "bird": [ {
+            "img": "https://headsupfortails.com/cdn/shop/products/ZVP-0216_Economic_falista_2013kopia.jpg?v=1654583481&width=360",
+                "name": "Vitapol Budgies",
+                "rating": 5.0,
+                "price": 169
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/ZuPreem82020FruitBlendMedBirds2lbFront762177820200.jpg?v=1654578331&width=360",
+                "name": "Fruit Blend",
+                "rating": 5.0,
+                "price": 215
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/ZVP-2768-768x960.jpg?v=1654583807&width=360",
+                "name": "Big Parrot Food",
+                "rating": 4.0,
+                "price": 224
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/ZuPreem85030NutBlendML_LargeBirs3_25lb762177850306.jpg?v=1654581982&width=360",
+                "name": "ZuPreem NutBleand",
+                "rating": 4.8,
+                "price": 599
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/Vitapol-Economic-Food-For-Cockatiel---1.2-kg.jpg?v=1646979811&width=360",
+                "name": "Cockateil Food",
+                "rating": 3.6,
+                "price": 325
+        }
+
+        ,
+            {
+            "img": "https://headsupfortails.com/cdn/shop/products/ZuPreem84030FruitBlendLargeBirds3_5lbFront762177840307.jpg?v=1654581765&width=360",
+                "name": "Conures Food",
+                "rating": 5.0,
+                "price": 125
+        }
+
+        ]
+    }
+}
+// code for counting of number meals served using setInterval function
+
+let num1 = document.getElementById("num");
+let num2 = document.getElementById("num2"); 
+
+let uniqueId = null;
+
+let counter = 0;
+uniqueId = setInterval(function() {
+    num1.textContent = counter;
+    num2.textContent = counter;
+    counter = counter + 808;
+    if (counter >= 34391) {
+        clearInterval(uniqueId);
+    }
+}, 40);
+
+let dogToyCategoriesEl = document.getElementById("dogToyCategories");
+
+let forName = document.getElementById("forName");
+let forRating = document.getElementById("forRating");
+let forPrice = document.getElementById("forPrice");
+
+
+//appending each item of product 
+function createAndAppendToys(eachItem) {
+    let {
+        img,
+        name,
+        rating,
+        price
+    } = eachItem;
+    console.log(img);
+
+    //bootstrap grid container
+    let divCont = document.createElement("div");
+    divCont.classList.add("col-6", "col-md-4");
+    dogToyCategoriesEl.appendChild(divCont);
+
+    //image element
+    let imgEl = document.createElement("img");
+    imgEl.src = img;
+    imgEl.classList.add("w-100", "m-3", "img-style")
+    divCont.appendChild(imgEl);
+
+    //nameEl
+    let nameEl = document.createElement("h6");
+    nameEl.textContent = name;
+    nameEl.classList.add("dog-toy-name-style");
+    divCont.appendChild(nameEl);
+
+    //rating Element
+    let ratingEl = document.createElement("p");
+    ratingEl.textContent = rating;
+    ratingEl.classList.add("dog-toy-name-style")
+    divCont.appendChild(ratingEl);
+
+    //price element
+    let priceEl = document.createElement("p");
+    priceEl.textContent = "₹" + price;
+    priceEl.classList.add("dog-toy-name-style")
+    divCont.appendChild(priceEl);
+}
+
+function fetchHardcodedData() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          json: () => Promise.resolve(hardcodedJSON),
+        });
+      }, 100);
+    });
+  }
+
+//Products data of Dog
+function getData(item) {
+
+    for (let eachItem of item) {
+        createAndAppendToys(eachItem);
+    }
+}
+
+function getDogToysList() {
+    dogToyCategoriesEl.textContent = "";
+
+    fetchHardcodedData()
+        .then(response => response.json())
+        .then(data => {
+            getData(data.dogs.toys);
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+
+function getDogClothingList() {
+    dogToyCategoriesEl.textContent = "";
+
+    fetchHardcodedData()
+        .then(response => response.json())
+        .then(data => {
+            getData(data.dogs.clothing);
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+//Products data for Cats
+
+function getCatBedList() {
+    dogToyCategoriesEl.textContent = "";
+
+    fetchHardcodedData()
+        .then(response => response.json())
+        .then(data => {
+            getData(data.cat.catBedding);
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+
+function getCatBowlsList() {
+    dogToyCategoriesEl.textContent = "";
+
+    fetchHardcodedData()
+        .then(response => response.json())
+        .then(data => {
+            getData(data.cat.catBowls);
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+//Products data for Rabbit
+
+function getRabbitFoodList() {
+    dogToyCategoriesEl.textContent = "";
+
+    fetchHardcodedData()
+        .then(response => response.json())
+        .then(data => {
+            getData(data.smallAnimals.rabbit);
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+//Products data for Birds
+
+function getBirdFoodList() {
+    dogToyCategoriesEl.textContent = "";
+
+    fetchHardcodedData()
+        .then(response => response.json())
+        .then(data => {
+            getData(data.smallAnimals.bird);
+        })
+        .catch(error => console.error('Error:', error));
+}
